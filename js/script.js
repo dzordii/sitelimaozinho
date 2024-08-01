e939d01b6cd6d4925069e73337f93470a23c5378
Isaac Dzordi <dzordinetwork@gmail.com>
2024-08-01 14:30:58 -0300

    Fix Vsl Player

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const checkoutButton = document.getElementById('checkoutButton');
 const myVideo = document.getElementById('myVideo');
 const playButton = document.getElementById('playButton');
 const videoContainer = document.getElementById('videoContainer');
+const playOverlay = document.getElementById('playOverlay');
 
 // Função para pausar ou reproduzir o vídeo
 function togglePlay() {
@@ -15,16 +16,9 @@ function togglePlay() {
 // Adiciona um evento de clique ao vídeo
 myVideo.addEventListener('click', togglePlay);
 
-// Adiciona um evento de clique ao botão
-playButton.addEventListener('click', togglePlay);
-
 // Adiciona um evento para mostrar/esconder o botão quando o vídeo é pausado/reproduzido
 myVideo.addEventListener('play', function() {
     videoContainer.classList.remove('video-paused');
-});
-
-myVideo.addEventListener('pause', function() {
-    videoContainer.classList.add('video-paused');
 });
 
 // Tenta reproduzir o vídeo automaticamente quando a página é carregada
@@ -39,4 +33,26 @@ myVideo.addEventListener('timeupdate', function() {
     if (myVideo.currentTime >= 15) {
         checkoutButton.style.display = 'block';
     }
+});
+
+// Função para alternar a visibilidade da sobreposição e a reprodução do vídeo
+function togglePlay() {
+    if (myVideo.paused) {
+        myVideo.play();
+        playOverlay.style.display = 'none'; // Oculta a sobreposição
+    } else {
+        myVideo.pause();
+        playOverlay.style.display = 'flex'; // Mostra a sobreposição
+    }
+}
+
+// Adiciona um evento de clique à sobreposição
+playOverlay.addEventListener('click', togglePlay);
+
+// Adiciona um evento de clique ao vídeo para alternar a sobreposição
+myVideo.addEventListener('click', togglePlay);
+
+// Mostra a sobreposição quando o vídeo é pausado
+myVideo.addEventListener('pause', function() {
+    playOverlay.style.display = 'flex';
 });
