4a9a046087f36a397a4f4f1d2cefacf7e9983443
Isaac Dzordi <dzordinetwork@gmail.com>
2024-08-07 23:45:39 -0300

    All change

--- README.md
+++
@@ -1,2 +0,0 @@
-# limao-site
-# sitelimaozinho
---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('ctaButton').addEventListener('click', function() {
+    alert('Você clicou em Envie-me o ebook!');
+    // Add your functionality here
+});
